f526c96aabbb97f167bc74b1be19f605b6323a89
areeshalam <areeshpersonal5@gmail.com>
2026-04-03 12:00:00 +1100

    Added: Updated the Styles

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { UserSettingsLayout } from "./layouts/UserSettingsLayout";
 export { ChannelSettingsLayout } from "./layouts/ChannelSettingsLayout";
 export { GeneralSettingsOverviewLayout } from "./layouts/GeneralSettingsOverviewLayout";
 export { VideoUploadSettingsLayout } from "./layouts/VideoUploadSettingsLayout";
+export { PricingSettingsLayout } from "./layouts/PricingSettingsLayout";
